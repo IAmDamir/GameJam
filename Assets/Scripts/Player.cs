e7d19d3ca861dd1fb67163eb26312ab63241382d
Damir <temirgalievdamir03@gmail.com>
2022-12-08 15:56:48 +0600

    Cleaned code/added some features

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,40 +1,62 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    private AudioSys audio;
-
     public GameObject playerModel;
 
-    public AnimationSys anim;
+    [Header("Systems")]
+    private AudioSys audio;
 
+    public AnimationSys anim;
     private InputSys inputSys;
+
+    [Header("Movement")]
+    public GameObject frwrd;
+
+    private Vector3 rght;
     public Rigidbody rb;
     public float speed;
 
+    [Header("Dash")]
     private DashPerk dash;
+
     public float dashDuration;
     public float dashCooldown;
 
+    [Header("Bow")]
     private BowPerk bow;
+
     public float bowDuration;
     public float bowCooldown;
+    [SerializeField] private ProjectileSpawner projSpawner;
+    [SerializeField] private Transform projSpawnerTransform;
 
-    public GameObject frwrd;
-    private Vector3 rght;
+    private void Awake()
+    {
+        inputSys = GetComponent<InputSys>();
+
+        inputSys.GetControls().Actions.Bow.performed += BowUse;
+    }
+
+    private void BowUse(InputAction.CallbackContext ctx)
+    {
+        bow.Action(true);
+        bow.Rotate();
+    }
 
     // Start is called before the first frame update
     private void Start()
     {
         audio = GetComponent<AudioSys>();
-        inputSys = GetComponent<InputSys>();
         rb = GetComponent<Rigidbody>();
         rght = Quaternion.Euler(new Vector3(0, 90, 0)) * frwrd.transform.forward;
 
         dash = new DashPerk(dashDuration, dashCooldown, inputSys, rb, speed);
-        bow = new BowPerk(bowDuration, bowCooldown, inputSys, anim, playerModel);
+        bow = new BowPerk(bowDuration, bowCooldown, inputSys, anim, playerModel, projSpawner, projSpawnerTransform);
     }
 
     // Update is called once per frame
@@ -60,8 +82,7 @@ public class Player : MonoBehaviour
         //Dash
         inputSys.Dash();
         dash.Action(inputSys._dash);
-        //Bow
-        inputSys.Bow();
-        bow.Action(inputSys._bow);
+
+        bow.Action(false);
     }
 }
