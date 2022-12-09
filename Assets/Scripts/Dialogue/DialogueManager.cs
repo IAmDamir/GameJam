686d0f9ad98f2656d056ce455d292fc2a3b81166
Damir <temirgalievdamir03@gmail.com>
2022-12-09 07:56:01 +0600

    Adding Dialogue System

---
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,124 @@
+using Ink.Runtime;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    [Header("Dialogue UI")]
+    [SerializeField] private GameObject dialoguePanel;
+
+    [Header("Portrait UI")]
+    [SerializeField] private GameObject portraitPanel;
+
+    [SerializeField] private TextMeshProUGUI dialogueText;
+    [SerializeField] private TextMeshProUGUI displayNameText;
+    [SerializeField] private Animator portraitAnimator;
+
+    private Story currentStory;
+    public bool dialogueIsPlaying { get; private set; }
+    private InputSys inputSystem;
+
+    private static DialogueManager instance;
+
+    private const string SPEAKER_TAG = "speaker";
+    private const string PORTRAIT_TAG = "portrait";
+    private const string SUBDIALOGUE_TAG = "subdialogue";
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("More than one DIalogue Manager instance in scene!");
+        }
+        instance = this;
+    }
+
+    public static DialogueManager GetInstance()
+    {
+        return instance;
+    }
+
+    private void Start()
+    {
+        dialogueIsPlaying = false;
+        dialoguePanel.SetActive(false);
+        portraitPanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (!dialogueIsPlaying)
+        {
+            return;
+        }
+
+        if (inputSystem.Submit())
+        {
+            ContinueStory();
+        }
+    }
+
+    public void EnterDialogueMode(TextAsset inkJSON)
+    {
+        currentStory = new Story(inkJSON.text);
+        dialogueIsPlaying = true;
+        dialoguePanel.SetActive(true);
+        portraitPanel.SetActive(true);
+
+        ContinueStory();
+    }
+
+    private void ExitDialogueMode()
+    {
+        dialogueIsPlaying = false;
+        dialoguePanel.SetActive(false);
+        portraitPanel.SetActive(false);
+        dialogueText.text = "";
+    }
+
+    private void ContinueStory()
+    {
+        if (currentStory.canContinue)
+        {
+            dialogueText.text = currentStory.Continue();
+
+            HandleTags(currentStory.currentTags);
+        }
+        else
+        {
+            ExitDialogueMode();
+        }
+    }
+
+    private void HandleTags(List<string> currentTags)
+    {
+        foreach (string tag in currentTags)
+        {
+            string[] splitTag = tag.Split(':');
+            if (splitTag.Length != 2)
+            {
+                Debug.Log("Tag cannot be separated " + tag);
+            }
+
+            string tagKey = splitTag[0].Trim();
+            string tagValue = splitTag[1].Trim();
+
+            switch (tagKey)
+            {
+                case SPEAKER_TAG:
+                    displayNameText.text = tagValue;
+                    break;
+
+                case PORTRAIT_TAG:
+                    portraitAnimator.Play(tagValue);
+                    break;
+
+                default:
+                    Debug.LogWarning(tagKey);
+                    break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ShaderGraph;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    [Header("Visual Cue")]
+    [SerializeField] private GameObject visualCue;
+
+    [Header("Ink JSON")]
+    [SerializeField] private TextAsset inkJSON;
+
+    private bool playerInRange;
+
+    private InputSys inputSys;
+
+    private void Awake()
+    {
+        playerInRange = false;
+        visualCue.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (playerInRange)
+        {
+            visualCue.SetActive(true);
+
+            if (inputSys.Interact())
+            {
+                DialogueManager.GetInstance().EnterDialogueMode(inkJSON);
+            }
+        }
+        else
+        {
+            visualCue.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            playerInRange = false;
+        }
+    }
+}
